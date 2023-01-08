87548849413f688424e40053c2fb57218e81161c
YAMADA Kazuaki <iterable.company@gmail.com>
2023-01-08 15:30:09 +0900

    implements exercise of 17.3

--- oop_concept/src/main.rs
+++ oop_concept/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     average();
     trait_object();
+    blog();
 }
 
 fn average() {
@@ -111,5 +112,136 @@ struct SelectBox {
 impl Draw for SelectBox {
     fn draw(&self) {
         todo!()
+    }
+}
+
+fn blog() {
+    let mut post = Post::new();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.rquest_review();
+    post.add_text("added after review requested");
+    assert_eq!("", post.content());
+
+    post.reject();
+    post.add_text(" added after review rejected");
+    post.rquest_review();
+
+    post.approve();
+    assert_eq!(
+        "I ate a salad for lunch today added after review rejected",
+        post.content()
+    );
+}
+
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        if self.state.as_ref().unwrap().can_add_text() {
+            self.content.push_str(text);
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+
+    pub fn rquest_review(&mut self) {
+        // take()はself.stateから所有権を奪い代わりにNoneにする
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.rquest_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn reject(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.reject())
+        }
+    }
+}
+
+trait State {
+    fn rquest_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&'a self, _: &'a Post) -> &'a str {
+        ""
+    }
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+    fn can_add_text(&self) -> bool {
+        false
+    }
+}
+
+struct Draft {}
+
+struct PendingReview {}
+
+struct Published {}
+
+impl State for Draft {
+    fn rquest_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn can_add_text(&self) -> bool {
+        true
+    }
+}
+
+impl State for PendingReview {
+    fn rquest_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
+}
+
+impl State for Published {
+    fn rquest_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&'a self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
     }
 }
