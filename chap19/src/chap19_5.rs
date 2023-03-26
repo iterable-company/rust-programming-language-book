1c190a8b6ca579b25dd4aa7d6eabdb00b79f7222
YAMADA Kazuaki <iterable.company@gmail.com>
2023-03-26 10:51:44 +0900

    19.5

---
+++ chap19/src/chap19_5.rs
@@ -0,0 +1,9 @@
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
+pub fn run() {
+    Pancakes::hello_macro();
+}
--- chap19/src/main.rs
+++ chap19/src/main.rs
@@ -2,10 +2,12 @@ pub mod chap19_1;
 pub mod chap19_2;
 pub mod chap19_3;
 pub mod chap19_4;
+pub mod chap19_5;
 
 fn main() {
     chap19_1::run();
     chap19_2::run();
     chap19_3::run();
     chap19_4::run();
+    chap19_5::run();
 }
