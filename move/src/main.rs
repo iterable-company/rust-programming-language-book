0d56de3908419d0f9ca1bc220d5cb7062b6320a1
YAMADA Kazuaki <iterable.company@gmail.com>
2023-06-24 17:31:02 +0900

    refactor

--- move/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1;
-    println!("{}, world!", s2);
-
-    let s3 = String::from("hello");
-    let s4 = s3.clone();
-    println!("{}, {}", s3, s4);
-}
--- usize/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-  let mut us: usize = 0;    // usizeは負の値を割り当てられない
-  us -= 1;                  // usizeが引き算して負の値になるときは、panic が起きる
-  println!("{}", us); 
-}
